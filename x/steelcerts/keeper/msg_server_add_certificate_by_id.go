7cbf62e5f67504094f479c9bb9321ea4e1e687c9
olaeseane <olaesean@gmail.com>
2021-12-18 23:46:22 +0300

    Handle add-certificate-by-id message

--- x/steelcerts/keeper/msg_server_add_certificate_by_id.go
+++ x/steelcerts/keeper/msg_server_add_certificate_by_id.go
@@ -4,14 +4,29 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/sapcis/steelcerts/x/steelcerts/types"
 )
 
 func (k msgServer) AddCertificateById(goCtx context.Context, msg *types.MsgAddCertificateById) (*types.MsgAddCertificateByIdResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
-
+	_, isFound := k.GetCertificate(ctx, msg.GetId())
+	if isFound {
+		return nil, sdkerrors.Wrap(types.ErrRecordIsExist, "The certificate already exists")
+	}
+	// creatorAddr, err := sdk.AccAddressFromBech32(msg.GetCreator())
+	// if err != nil {
+	// 	return nil, sdkerrors.Wrap(types.ErrGetCreator, "Get creator error") 
+	// }
+	newCert := types.Certificate{
+		Index:       msg.GetId(),
+		Companycode: msg.GetCreator(), // TODO: Define name of wallet holder
+		Productname: msg.GetProductname(),
+		Status:      msg.GetStatus(),
+		Url:         msg.GetUrl(),
+		Checkcsum:   msg.GetChecksum(),
+	}
+	k.SetCertificate(ctx, newCert)
 	return &types.MsgAddCertificateByIdResponse{}, nil
 }
--- x/steelcerts/types/errors.go
+++ x/steelcerts/types/errors.go
@@ -8,5 +8,7 @@ import (
 
 // x/steelcerts module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	// ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrRecordIsExist = sdkerrors.Register(ModuleName, 1100, "The certificate already exists")
+	ErrGetCreator    = sdkerrors.Register(ModuleName, 1101, "Get creator error")
 )
